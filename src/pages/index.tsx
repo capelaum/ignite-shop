9396a9ba4d9a3ae5ce2fd85266e3484e22488349
Luis V. Capelletto <thecapellett@gmail.com>
2022-09-16 16:39:55 -0300

    🔧 config stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
+import Head from 'next/head'
+import { styled } from 'styles'
+
+const Button = styled('button', {
+  backgroundColor: '$green',
+  borderRadius: 7,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    backgroundColor: '$greenLight',
+  },
+})
+
 export default function Home() {
-  return <h1>Hello Next</h1>
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <h1>Hello Next</h1>
+
+      <Button>
+        <span>Click me</span>
+      </Button>
+    </>
+  )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,24 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  theme,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      primary: '#1e1e1e',
+      green: '#00875F',
+      greenLight: '#00B37E',
+      gray1: '#121214',
+      gray2: '#202024',
+      gray3: '#C4C4CC',
+      gray4: '#E1E1E6',
+    },
+  },
+})
